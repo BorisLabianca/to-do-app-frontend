dfadfc405d09bb2570934c925f67421194fc8459
Boris Labianca <boris.labianca@hotmail.fr>
2023-09-26 23:12:17 +0800

    created basic components

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,5 +1,23 @@
+import Task from "./Task";
+import TaskForm from "./TaskForm";
+
 const TaskList = () => {
-  return <div>TaskList</div>;
+  return (
+    <div>
+      <h2>Task Manager</h2>
+      <TaskForm />
+      <div className="--flex-between --pb">
+        <p>
+          <b>Total Tasks:</b>
+        </p>
+        <p>
+          <b>Completed Tasks:</b>
+        </p>
+      </div>
+      <hr />
+      <Task />
+    </div>
+  );
 };
 
 export default TaskList;
