74e37e3ec297585bcead6a485d7a418b5545916e
Boris Labianca <boris.labianca@hotmail.fr>
2023-09-29 13:31:52 +0800

    added logic to fetch all tasks from database

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,11 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Task from "./Task";
 import TaskForm from "./TaskForm";
 import { toast } from "react-toastify";
 import axios from "axios";
 import { URL } from "../App";
+import loader from "../assets/loader.gif";
 
 const TaskList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [completedTasks, setCompletedTasks] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const [formData, setFormData] = useState({ name: "", completed: false });
   const { name } = formData;
 
@@ -13,6 +17,23 @@ const TaskList = () => {
     const { name, value } = event.target;
     setFormData({ ...formData, [name]: value });
   };
+
+  const getTasks = async () => {
+    setIsLoading(true);
+    try {
+      const { data } = await axios.get(`${URL}/api/tasks/all-tasks`);
+      setTasks(data);
+      console.log(data);
+      setIsLoading(false);
+    } catch (error) {
+      toast.error(error.message);
+      console.log(error);
+      setIsLoading(false);
+    }
+  };
+  useEffect(() => {
+    getTasks();
+  }, []);
 
   const createTask = async (event) => {
     event.preventDefault();
@@ -38,14 +59,29 @@ const TaskList = () => {
       />
       <div className="--flex-between --pb">
         <p>
-          <b>Total Tasks:</b>
+          <b>Total Tasks: {`${tasks.length}`}</b>
         </p>
         <p>
-          <b>Completed Tasks:</b>
+          <b>Completed Tasks: {`${tasks.length}`}</b>
         </p>
       </div>
       <hr />
-      <Task />
+      {isLoading && (
+        <div className="--flex-center">
+          <img src={loader} alt="Loading" />
+        </div>
+      )}
+      {!isLoading && tasks.length === 0 ? (
+        <p className="--py">
+          No tasks in your drawer. Please add a task to do.
+        </p>
+      ) : (
+        <>
+          {tasks.map((task, index) => {
+            return <Task key={task._id} task={task} index={index} />;
+          })}
+        </>
+      )}
     </div>
   );
 };
